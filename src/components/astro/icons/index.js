0a90efda551ef3d977df90f76aace95f6d4d71f5
NotToxic <47497223+NotToxicDev@users.noreply.github.com>
2022-07-08 14:06:14 +0200

    Fix footer

--- src/components/astro/icons/index.js
+++ src/components/astro/icons/index.js
@@ -1,17 +1,15 @@
 import IconSun from "./IconSun.astro"
 import IconGithub from "./IconGithub.astro"
 import IconTwitter from "./IconTwitter.astro"
-import IconFacebook from "./IconFacebook.astro"
-import IconInstagram from "./IconInstagram.astro"
 import IconArrowRight from "./IconArrowRight.astro"
 import IconMenu from "./IconMenu.astro"
+import IconYoutube from "./IconYoutube.astro"
 
 export {
   IconSun,
   IconGithub,
   IconTwitter,
-  IconFacebook,
-  IconInstagram,
   IconArrowRight,
-  IconMenu
+  IconMenu,
+  IconYoutube
 }
